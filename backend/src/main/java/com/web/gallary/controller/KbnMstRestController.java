a4bfd02066bd1acb103df748ea4f6b99a43c8b4d
KentoKodama214 <kodaken214@gmail.com>
2026-05-12 23:02:03 +0900

    KbnMstRestControllerのテストコードを作成

--- backend/src/main/java/com/web/gallary/controller/KbnMstRestController.java
+++ backend/src/main/java/com/web/gallary/controller/KbnMstRestController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,7 @@ import lombok.RequiredArgsConstructor;
  * @version	1.0.0
  * @since	1.0.0
  */
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class KbnMstRestController {
